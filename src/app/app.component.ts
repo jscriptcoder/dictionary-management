02f5e2d7abff204edc819ef43891778c2b179a2f
Fran <jscriptcoder@gmail.com>
2017-06-01 08:08:25 +0200

    Setting up angular material and adding routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
